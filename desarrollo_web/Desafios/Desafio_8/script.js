31c594d17d6bdf65bc43c28b11b1597de1c2d41a
ConstanzaMorales <constanza.morales.a@usach.cl>
2022-08-30 23:54:57 -0400

    first commi

--- desarrollo_web/Desafios/Desafio_8/script.js
+++ desarrollo_web/Desafios/Desafio_8/script.js
@@ -1,8 +1,15 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () =>{
-    navbarLinks.classList.toggle('active')
+$('.carousel').carousel({
+  interval: 2000
 })
+document.querySelector(".card-flip").classList.toggle("flip");
 
-
+/* 
+ * Holder.js for demo image
+ * Just for demo purpose
+ */
+Holder.addTheme('gray', {
+  bg: '#777',
+  fg: 'rgba(255,255,255,.75)',
+  font: 'Helvetica',
+  fontweight: 'normal'
+});
